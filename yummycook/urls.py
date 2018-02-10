0cb4966976917ebfd2f3cfd9d2d3d006506f7b12
Juan C. Alvarez <j.c.alvarez658@gmail.com>
2018-02-10 16:22:27 -0400

    update 2 login sign up

--- yummycook/urls.py
+++ yummycook/urls.py
@@ -19,11 +19,13 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^about/$', views.about),
     url(r'^$',views.homepage),
     url(r'^articles/', include('articles.urls')),
+    url(r'^accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
