a46186144129e8c52e209841a5d8d426c7b0fc3e
Juan C. Alvarez <j.c.alvarez658@gmail.com>
2018-02-12 16:10:22 -0400

    Blog completados

--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,7 @@ from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from . import forms
+
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
@@ -23,7 +24,9 @@ def article_create(request):
     if request.method == 'POST':
         form = forms.CreateArticle(request.POST, request.FILES)
         if form.is_valid():
-            #save article to db
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
             return redirect('articles:list')
     else:  
         form = forms.CreateArticle()
