4fc12051381b40259c5222fd65d9291c3565a96b
Juan C. Alvarez <j.c.alvarez658@gmail.com>
2018-04-08 11:09:34 -0400

    Add first consumer

--- yummycook/routing.py
+++ yummycook/routing.py
@@ -1,5 +1,13 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+import chatrooms.routing
 
 application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chatrooms.routing.websocket_urlpatterns
+        )
+    )
     
 })
